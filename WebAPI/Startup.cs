64d9c865c097c26796b0d0b2faa4c233f679c4c5
DaoHanh32 <77731242+DaoHanh32@users.noreply.github.com>
2022-01-17 15:04:55 +0700

    đổi file config trong program.cs

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -28,7 +28,7 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            string url = "server=LAPTOP-6D8AK342\\CHI;database=CustomerAttributeBlazor;uid=sa;pwd=123";
+            string url = "server=HAN\\SQLEXPRESS;database=CustomerAttributeBlazor;uid=sa;pwd=123";
             services.AddDbContext<CustomerAttributeContext>(options =>
             {
                 options.UseSqlServer(url);
