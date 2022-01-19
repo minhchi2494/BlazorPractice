6e6ca3f993d5afa2e665c8e99d7e3484d8567cc1
DaoHanh32 <77731242+DaoHanh32@users.noreply.github.com>
2022-01-19 11:56:14 +0700

    thêm phân trang thành công

--- BlazorApp/Pages/CustomerAttributeView.razor.cs
+++ BlazorApp/Pages/CustomerAttributeView.razor.cs
@@ -18,17 +18,20 @@ namespace BlazorApp.Pages
         private List<CustomerAttributeModel> model;
         private CustomerSearch CustomerSearch = new CustomerSearch();
         protected Confirmation DeleteConfirmation { set; get; }
+        public MetaData MetaData { get; set; } = new MetaData(); 
 
         private int DeleteId { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-            model = await _services.getAll(CustomerSearch);
+            //model = await _services.getAll(CustomerSearch);
+            await GetTask();
         }
 
         private async Task SeachForm(EditContext context)
         {
-            model = await _services.getAll(CustomerSearch);
+            //model = await _services.getAll(CustomerSearch);
+            await GetTask();
         }
 
         public void OnDeleteTask(int deleteId)
@@ -42,8 +45,21 @@ namespace BlazorApp.Pages
             if (deleteConfirmed)
             {
                 await _services.Delete(DeleteId);
-                model = await _services.getAll(CustomerSearch);
+                // model = await _services.getAll(CustomerSearch);
+                await GetTask();
             }
+        }
+
+        private async Task GetTask()
+        {
+            var pagingRespone = await _services.getAll(CustomerSearch);
+            model = pagingRespone.Items;
+            MetaData = pagingRespone.MetaData;
+        }
+        private async Task SelectedPage(int page)
+        {
+            CustomerSearch.PageNumber = page;
+            await GetTask();
         }
     }
 }
