0fbce636c6ca9eb2d9d201f3ca9a4f764a858165
DaoHanh32 <77731242+DaoHanh32@users.noreply.github.com>
2022-01-19 13:59:54 +0700

    đang làm dở valid

---
+++ BlazorApp/Pages/CustomValidatorAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Pages
+{
+    public class CustomValidatorAttribute : ValidationAttribute
+    {
+        public string ValidDescription { get; set; }
+        public string ValidShortName { get; set; }
+
+    }
+}
